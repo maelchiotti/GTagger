2524c6694cbe484f4505a6db4e829b14fdad9212
Maël Chiotti <mael.chiotti@gmail.com>
2022-04-25 14:07:39 +0200

    reformat

--- geniuslyrics.py
+++ geniuslyrics.py
@@ -7,68 +7,10 @@ import eyed3
 import lyricsgenius
 
 
-# Output colors
-class colors:
-    PURPLE = '\033[95m'
-    GREEN = '\033[92m'
-    ORANGE = '\033[93m'
-    RED = '\033[91m'
-    BOLD = '\033[1m'
-    ENDC = '\033[0m'
-
-
-# Format the lyrics (remove unwanted text)
-def format_lyrics(lyrics):
-    lines = lyrics.split("\n")
-    if(len(lines) > 0):
-        lines.pop(0)
-    if(len(lines) > 1):
-        lines[len(lines) - 1] = re.sub("[0-9]+Embed",
-                                       "", lines[len(lines) - 1])
-    lyrics = "\n".join(lines)
-    return lyrics
-
-
 def main():
     print(colors.PURPLE + colors.BOLD + "GeniusLyrics | Start\n" + colors.ENDC)
-
-    # Check number of arguments
-    if(len(sys.argv) < 3):
-        print(colors.BOLD +
-              "Usage:" + colors.ENDC + "\n\tpy geniuslyrics.py <Genius client ID> <tracks folder path> <options>")
-        print(colors.BOLD + "Options:" + colors.ENDC +
-              "\n\t-o\tOverwrite already existing lyrics")
-        exit(-1)
-
-    # Client ID and Genius instance
-    clientID = sys.argv[1]
-    search = re.search("[^a-zA-Z0-9_]", clientID)
-    if(search != None):
-        print("Incorrect client ID: ")
-        print(search)
-        exit(-1)
-    genius = lyricsgenius.Genius(clientID)
-    genius.verbose = False
-    genius.remove_section_headers = True
-
-    # Path name
-    pathname = sys.argv[2]
-    if(not pathname.endswith("\\")):
-        pathname = pathname + "\\"
-    if(not os.path.isdir(pathname)):
-        print("Incorrect path name: " + pathname)
-
-    # Check for options
-    options = {"-o": False}
-    for option in options.keys():
-        for i in range(3, len(sys.argv)):
-            if option == sys.argv[i]:
-                options[option] = True
-
-    # Counters
-    lyrics_total = 0
-    lyrics_found = 0
-    lyrics_not_saved = 0
+    check_args()
+    genius, pathname, options, lyrics_total, lyrics_found, lyrics_not_saved = init()
 
     for filename in glob.glob(pathname + "*.mp3"):
         audiofile = eyed3.load(filename)
@@ -129,20 +71,92 @@ def main():
                 continue
 
     # Statistics
-    lyrics_found_perc = (lyrics_found - lyrics_not_saved) / lyrics_total * \
-        100 if (lyrics_total > 0) else "-"
-    lyrics_not_found_perc = (lyrics_total - lyrics_found) / \
-        lyrics_total * 100 if (lyrics_total > 0) else "-"
-    lyrics_not_saved_perc = lyrics_not_saved / \
-        lyrics_total * 100 if (lyrics_total > 0) else "-"
-    print(colors.PURPLE + colors.BOLD + "\nGeniusLyrics | End\n" + colors.ENDC)
-    print(colors.BOLD + str(lyrics_total) + " lyrics searched" + colors.ENDC)
-    print(colors.GREEN + "%.2f%% lyrics found and saved" %
-          lyrics_found_perc + colors.ENDC)
-    print(colors.ORANGE + "%.2f%% lyrics not found" %
-          lyrics_not_found_perc + colors.ENDC)
-    print(colors.RED + "%.2f%% lyrics found but not saved" %
-          lyrics_not_saved_perc + colors.ENDC)
+    print(colors.PURPLE + colors.BOLD +
+          "\nGeniusLyrics | End\n" + colors.ENDC)
+    if(lyrics_total > 0):
+        lyrics_found_perc = (
+            lyrics_found - lyrics_not_saved) / lyrics_total * 100
+        lyrics_not_found_perc = (
+            lyrics_total - lyrics_found) / lyrics_total * 100
+        lyrics_not_saved_perc = lyrics_not_saved / lyrics_total * 100
+        print(colors.BOLD + str(lyrics_total) +
+              " lyrics searched" + colors.ENDC)
+        print(colors.GREEN + "%.2f%% lyrics found and saved" %
+              lyrics_found_perc + colors.ENDC)
+        print(colors.ORANGE + "%.2f%% lyrics not found" %
+              lyrics_not_found_perc + colors.ENDC)
+        print(colors.RED + "%.2f%% lyrics found but not saved" %
+              lyrics_not_saved_perc + colors.ENDC)
+    else:
+        print("No lyrics searched")
+
+
+# Check arguments
+def check_args():
+    # Check number of arguments
+    if(len(sys.argv) < 3):
+        print(colors.BOLD +
+              "Usage:" + colors.ENDC + "\n\tpy geniuslyrics.py <Genius client ID> <tracks folder path> <options>")
+        print(colors.BOLD + "Options:" + colors.ENDC +
+              "\n\t-o\tOverwrite already existing lyrics")
+        exit(-1)
+
+
+# Initialize variables
+def init():
+    # Client ID and Genius instance
+    clientID = sys.argv[1]
+    search = re.search("[^a-zA-Z0-9_]", clientID)
+    if(search != None):
+        print("Incorrect client ID: ")
+        print(search)
+        exit(-1)
+    genius = lyricsgenius.Genius(clientID)
+    genius.verbose = False
+    genius.remove_section_headers = True
+
+    # Path name
+    pathname = sys.argv[2]
+    if(not pathname.endswith("\\")):
+        pathname = pathname + "\\"
+    if(not os.path.isdir(pathname)):
+        print("Incorrect path name: " + pathname)
+
+    # Check for options
+    options = {"-o": False}
+    for option in options.keys():
+        for i in range(3, len(sys.argv)):
+            if option == sys.argv[i]:
+                options[option] = True
+
+    # Counters
+    lyrics_total = 0
+    lyrics_found = 0
+    lyrics_not_saved = 0
+
+    return genius, pathname, options, lyrics_total, lyrics_found, lyrics_not_saved
+
+
+# Format the lyrics (remove unwanted text)
+def format_lyrics(lyrics):
+    lines = lyrics.split("\n")
+    if(len(lines) > 0):
+        lines.pop(0)
+    if(len(lines) > 1):
+        lines[len(lines) - 1] = re.sub("[0-9]+Embed",
+                                       "", lines[len(lines) - 1])
+    lyrics = "\n".join(lines)
+    return lyrics
+
+
+# Output colors
+class colors:
+    PURPLE = '\033[95m'
+    GREEN = '\033[92m'
+    ORANGE = '\033[93m'
+    RED = '\033[91m'
+    BOLD = '\033[1m'
+    ENDC = '\033[0m'
 
 
 if __name__ == "__main__":
