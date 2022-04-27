9ba6f4b69a0034f848c44488613c0914da9c9ee1
Maël Chiotti <mael.chiotti@gmail.com>
2022-04-27 17:29:57 +0200

    ADD tags : title, artist, date, cover art, renaming

--- geniuslyrics.py
+++ geniuslyrics.py
@@ -2,14 +2,28 @@ import os
 import sys
 import re
 import glob
+import urllib
 import eyed3
+from eyed3.id3.frames import ImageFrame
+import eyed3.core
 import lyricsgenius
-from genius import Genius
+import genius
+
+
+# Pretend to be a request form a normal web brower, otherwise Genius doesn't allow downloading of cover arts
+HEADER = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) '
+          'AppleWebKit/537.11 (KHTML, like Gecko) '
+          'Chrome/23.0.1271.64 Safari/537.11',
+          'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+          'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+          'Accept-Encoding': 'none',
+          'Accept-Language': 'en-US,en;q=0.8',
+          'Connection': 'keep-alive'}
 
 
 def main():
     check_args()
-    genius, pathname, options, lyrics_total, lyrics_found, lyrics_not_saved = init()
+    g, lg, pathname, options, lyrics_total, lyrics_found, lyrics_not_saved = init()
     print(colors.PURPLE + colors.BOLD + "GeniusLyrics | Start\n" + colors.ENDC)
 
     for filename in glob.glob(pathname + "*.mp3"):
@@ -38,33 +52,69 @@ def main():
                 artist = artist.split(splitter)[0]
                 break
 
-        # Look for the track (while true to fix the timeout error)
-        while True:
-            try:
-                track = genius.search_song(title, artist)
-                break
-            except:
-                pass
+        # Search for the track and its lyrics
+        ltrack = None
+        searched_tracks = g.search(title + " " + artist)
+        try:
+            searched_track = next(searched_tracks)
+            if(searched_track != None):
+                track = g.get_song(searched_track.id)
+                if(track != None):
+                    ltrack = lg.search_song(
+                        song_id=searched_track.id, get_full_info=False)
+        except StopIteration:
+            print(colors.RED + "The track \"" +
+                  title + "\" by " + artist + " could not be found on Genius" + colors.ENDC)
+            continue
 
-        if(track != None):
+        if(ltrack != None):
+            # Set the main tags
+            audiofile.tag.title = track.title
+            audiofile.tag.artist = track.artist.name
+            if(track.release_date != None):
+                audiofile.tag.recording_date = eyed3.core.Date(
+                    track.release_date.year)
+            if(track.album != None):
+                audiofile.tag.album = track.album.name
+            else:
+                audiofile.tag.album = track.title + " - Single"
+
+            # Find the cover art URL (either the album one or otherwise the track one), download it and set it
+            url = ""
+            if(track.album != None):
+                url = track.album.cover_art_url
+            else:
+                url = track.song_art_image_url
+            request = urllib.request.Request(url=url, headers=HEADER)
+            imagedata = urllib.request.urlopen(request).read()
+            audiofile.tag.images.set(
+                ImageFrame.FRONT_COVER, imagedata, "image/jpeg")
+
             # Set the lyrics
-            lyrics = format_lyrics(track.lyrics)
+            lyrics = format_lyrics(ltrack.lyrics)
             audiofile.tag.lyrics.set(lyrics)
             lyrics_found += 1
             print(colors.GREEN + "Lyrics found for \"" +
                   title + "\" by " + artist + colors.ENDC)
 
-            # Save the lyrics
+            # Save the tags
             try:
                 audiofile.tag.save(
-                    version=eyed3.id3.ID3_DEFAULT_VERSION, encoding='utf-8')
+                    version=eyed3.id3.ID3_V2_3, encoding='utf-8')
 
             # TagException is raised when an error while saving occurs
             except eyed3.id3.tag.TagException:
                 lyrics_not_saved += 1
                 print(colors.RED + "Lyrics could not be saved for \"" +
                       title + "\" by " + artist + colors.ENDC)
-                continue
+                pass
+
+            # Rename the file
+            try:
+                audiofile.rename(audiofile.tag.title + " - " +
+                                 audiofile.tag.artist, preserve_file_time=True)
+            except IOError:
+                pass
 
         else:
             audiofile.tag.lyrics.set("")
@@ -93,15 +143,17 @@ def print_help():
 
 # Initialize variables
 def init():
-    # Access token and Genius instance
+    # Access token and
     access_token = sys.argv[1]
     search = re.search("[^a-zA-Z0-9_]", access_token)
     if(search != None):
         print("Incorrect access token")
         print_help()
-    genius = lyricsgenius.Genius(access_token)
-    genius.verbose = False
-    genius.remove_section_headers = True
+
+    # genius and lyricsgenius instances
+    g = genius.Genius(access_token=access_token)
+    lg = lyricsgenius.Genius(
+        access_token=access_token, verbose=False, remove_section_headers=True)
 
     # Path name
     pathname = sys.argv[2]
@@ -122,7 +174,7 @@ def init():
     lyrics_found = 0
     lyrics_not_saved = 0
 
-    return genius, pathname, options, lyrics_total, lyrics_found, lyrics_not_saved
+    return g, lg, pathname, options, lyrics_total, lyrics_found, lyrics_not_saved
 
 
 # Format the lyrics (remove unwanted text)
