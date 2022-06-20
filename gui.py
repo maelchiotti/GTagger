88d0059e5a051247465077fc9902c564a0983793
Maël Chiotti <mael.chiotti@gmail.com>
2022-06-20 23:37:16 +0200

    track and lyrics search work

--- gui.py
+++ gui.py
@@ -7,6 +7,8 @@ import sys
 import pathlib
 from PySide6 import QtCore, QtWidgets
 
+from tools import Track, TrackSearch, LyricsSearch
+
 class MainWindow(QtWidgets.QWidget):
     """
     Main window of the GUI
@@ -14,9 +16,11 @@ class MainWindow(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
 
+        self.tracks = []
+
         self.table = QtWidgets.QTableWidget()
-        self.table.setColumnCount(3)
-        self.table.setHorizontalHeaderLabels(["File", "Artist", "Title"])
+        self.table.setColumnCount(4)
+        self.table.setHorizontalHeaderLabels(["File", "Title", "Main artist", "Lyrics"])
 
         self.button_add_directories = QtWidgets.QPushButton("Add directory")
         self.button_add_files = QtWidgets.QPushButton("Add files")
@@ -29,14 +33,14 @@ class MainWindow(QtWidgets.QWidget):
         self.button_add_directories.clicked.connect(lambda:self.add_files(True))
         self.button_add_files.clicked.connect(lambda:self.add_files(False))
 
-    def select_directories(self):
+    def select_directories(self) -> str:
         directory_dialog = QtWidgets.QFileDialog()
         directory = directory_dialog.getExistingDirectory()
         if directory == "":
             return None
         return directory
 
-    def select_files(self):
+    def select_files(self) -> list[str]:
         file_dialog = QtWidgets.QFileDialog()
         files = file_dialog.getOpenFileNames(caption="Select file(s)", filter="MP3 files (*.mp3)")
         if len(files[0]) == 0:
@@ -44,7 +48,7 @@ class MainWindow(QtWidgets.QWidget):
         return files[0]
 
     @QtCore.Slot()
-    def add_files(self, select_directory):
+    def add_files(self, select_directory: bool) -> None:
         if select_directory:
             directory = self.select_directories()
             if directory is None:
@@ -54,12 +58,23 @@ class MainWindow(QtWidgets.QWidget):
             files = self.select_files()
             if files is None:
                 return
-        
+
         for file in files:
-            filename = os.path.basename(file)
-            item = QtWidgets.QTableWidgetItem(filename)
+            track = Track(file)
+            self.tracks.append(track)
+
             self.table.insertRow(self.table.rowCount())
-            self.table.setItem(self.table.rowCount() - 1, 0, item)
+            self.table.setItem(self.table.rowCount() - 1, 0, QtWidgets.QTableWidgetItem(track.filename))
+            self.table.setItem(self.table.rowCount() - 1, 1, QtWidgets.QTableWidgetItem(track.title))
+            self.table.setItem(self.table.rowCount() - 1, 2, QtWidgets.QTableWidgetItem(track.main_artist))
+
+            #TODO ce sont juste des tests
+            track_search = TrackSearch("8hPJjsOhGDfGB5naUVwKMF7zGK5XCV5-pRsIu55LSQgK_5Yo2HTsBNJnWamF8GMk")
+            track_search.search_track(track)
+            lyrics_search = LyricsSearch("8hPJjsOhGDfGB5naUVwKMF7zGK5XCV5-pRsIu55LSQgK_5Yo2HTsBNJnWamF8GMk")
+            lyrics_search.search_lyrics(track)
+            self.table.setItem(self.table.rowCount() - 1, 3, QtWidgets.QTableWidgetItem(track.lyrics[:100]))
+
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication([])
---
+++ tools.py
@@ -0,0 +1,146 @@
+"""
+Tools helpful for the GUI
+"""
+
+import os
+import re
+import logging as log
+import eyed3
+import genius
+from genius import classes
+import lyricsgenius
+from lyricsgenius import types
+
+class Track():
+    """
+    Class representing a music track
+
+    Attributes:
+    filepath: str
+        filepath of the track
+    filename: str
+        filename of the track
+    title: str
+        title of the track
+    artist: list[str]
+        artist of the track
+    lyrics: str
+        lyrics of the track
+    """
+    SPLITTERS = " featuring | feat. | feat | ft. | ft | & | / "
+
+    def __init__(self, filepath: str) -> None:
+        self.filepath: str = filepath
+        self.filename: str = os.path.basename(filepath)
+        self.genius_tags: classes.Song = None
+        self.title: str = None
+        self.artists: list[str] = []
+        self.lyrics: str = None
+        self.read_tags()
+
+    def read_tags(self) -> None:
+        """
+        Use eyed3 to read the tags from the file and set them
+        """
+        tags = eyed3.load(self.filepath)
+        self.title = tags.tag.title
+        self.artists = re.split(self.SPLITTERS, tags.tag.artist)
+        self.main_artist = self.artists[0]
+
+class TrackSearch():
+    """
+    Class allowing to search a track on Genius
+    """
+    def __init__(self, token: str) -> None:
+        search = re.search("[^a-zA-Z0-9_-]", token)
+        if search is not None:
+            log.error("Incorrect access token: %s", search)
+            self.token: str = None
+            self.genius: genius.Genius = None
+        else:
+            self.token: str = token
+            self.genius: genius.Genius = genius.Genius(access_token=self.token)
+
+    def search_track(self, track: Track) -> bool:
+        """
+        Searches for a track on Genius
+        """
+        search = track.title + " " + track.main_artist
+        searched_tracks = self.genius.search(search)
+        try:
+            searched_track = next(searched_tracks)
+        except StopIteration:
+            log.warning("Track '%s' by %s not found on Genius", track.title, track.main_artist)
+            return False
+        track.genius_tags = searched_track
+        return True
+
+class LyricsSearch():
+    """
+    Class allowing to search a track's lyrics on Genius
+    """
+    def __init__(self, token: str) -> None:
+        if not Tools.is_token_correct(token):
+            self.token: str = None
+            self.lyrics_genius: lyricsgenius.Genius = None
+        else:
+            self.token: str = token
+            self.lyrics_genius: lyricsgenius.Genius = lyricsgenius.Genius(
+                access_token=self.token,
+                verbose=False,
+                remove_section_headers=True
+            )
+
+    def search_lyrics(self, track: Track) -> bool:
+        """
+        Searches for a track's lyrics on Genius
+        """
+        searched_track: types.Song = None
+        while True:
+            try:
+                searched_track = self.lyrics_genius.search_song(
+                    song_id=track.genius_tags.id,
+                    get_full_info=False
+                )
+                break
+            except Exception as exception:
+                log.error("Unexpected exception: %s", exception)
+                return False
+
+        if searched_track is None:
+            log.warning("Track '%s' lyrics by %s not found on Genius", track.title, track.main_artist)
+            return False
+
+        lyrics = Tools.format_lyrics(searched_track.lyrics)
+        track.lyrics = lyrics
+        return True
+
+
+class Tools():
+    """
+    Class containing miscellaneous tools
+    """
+    @staticmethod
+    def is_token_correct(token: str) -> bool:
+        """
+        Checks if a token has the correct syntax as only letters and digits are allowed
+        """
+        search = re.search("[^a-zA-Z0-9_-]", token)
+        if search is not None:
+            log.error("Incorrect access token: %s", search)
+            return False
+        else:
+            return True
+
+    @staticmethod
+    def format_lyrics(unformatted_lyrics: str) -> str:
+        """
+        Format the lyrics by removing unwanted text
+        """
+        lines = unformatted_lyrics.split("\n")
+        if len(lines) > 0:
+            lines.pop(0)
+        if len(lines) > 1:
+            lines[len(lines) - 1] = re.sub("[0-9]+Embed", "", lines[len(lines) - 1])
+        return "\n".join(lines)
+    
